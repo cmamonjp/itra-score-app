1798a9b525dc40388174dfe790ceff969d81eb59
cmamonjp <takumi.shimano@miraial.co.jp>
2025-05-26 13:51:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,10 +10,11 @@ if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
     st.write(df)
 
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(12, 6))
     ax.plot(pd.to_datetime(df['date']), df['itra_score'], marker='o')
     ax.set_title("ITRA Score Progress")
     ax.set_xlabel("Date")
     ax.set_ylabel("ITRA Score")
+    plt.xticks(rotation=60, fontsize=8)
 
     st.pyplot(fig)
